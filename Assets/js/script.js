40e607ecce4aeac98c38166f30673e984683fc6f
FatherDkay <paul.d.bailey@hotmail.com>
2021-09-25 16:26:33 -0700

    added location button function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,6 @@
 let searchBtnEl = document.querySelector("#search-button")
 let searchInputEl = document.querySelector("#search-value")
 let mainEl = document.querySelector("main")
-
 let bpmApi = {
     url: "https://api.getsongbpm.com/tempo/",
     api_key: "d6d1431d02fa9c2f938ab2a8074ef686"
@@ -10,16 +9,20 @@ let bpmApi = {
 ////START WEATHER FUNCTION////
 
 //Weater Variables//
-var weatherBtnEl = document.querySelector("#weather-button")
-var weatherInputEl = document.querySelector("#weather-value")
+var weatherBtnEl = document.querySelector("#weather-button");
+var weatherInputEl = document.querySelector("#weather-value");
+var locationBtnEl = document.querySelector("#location-button");
 var oneCallAPIKey = '9234bf92e34e7602754600bc3607c0dc';
 var googleAPIKey = 'AIzaSyCfm9Ok_M-qzamFmAAcDIVFNVKKTjpRvis';
 var searchCity;
 var curentWeather;
 var CityName;
 var iconPath = "http://openweathermap.org/img/wn/";
+var cityLocation;
+var latitude;
+var longitude;
 
-//Start Get Lat Lon From City Name
+//Start Get Lat Lng From City Name
 function weatherBtnHandler() {
     fetch('https://maps.googleapis.com/maps/api/geocode/json?key=' + googleAPIKey +'&address='+ weatherInputEl.value)
     .then(function(response) {
@@ -28,8 +31,10 @@ function weatherBtnHandler() {
             response.json().then(function(cityData) {
                 cityLocation = (cityData.results[0].geometry.location);
                 console.log(cityLocation);
+                latitude = cityLocation.lat;
+                longitude = cityLocation.lng;
                 getCityName();
-                getWeather();
+                getWeatherFromLocation();
             });
         } else {
             alert("Something went wrong.  Please try again");
@@ -39,17 +44,20 @@ function weatherBtnHandler() {
         alert("Unable to complete City Search function")
     });
 }
-//End Get Lat Lon From City Name
+//End Get Lat Lng From City Name
 
+//Start Get City Name From Search Box
+  //Not being used unless I can figure out reverse geocoding
  function getCityName () {
      cityNfo = weatherInputEl.value;
      cityInfo = cityNfo.split(",");
     cityName = cityInfo[0];
 }
+//End Get City Name From Search Box
 
-//Start Get Weather From Lat Lon
-function getWeather(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLocation.lat +'&lon=' + cityLocation.lng +'&units=imperial&excludeminutely,alerts&appid=' + oneCallAPIKey)
+//Start Get Weather Information
+function getWeatherFromLocation(){
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude +'&lon=' + longitude +'&units=imperial&excludeminutely,alerts&appid=' + oneCallAPIKey)
     .then(function(response){
         console.log(response);
         return response.json();
@@ -60,14 +68,16 @@ function getWeather(){
         populateWeather();
     })
 }
+//End Get Weather Information
+
+
 function populateWeather() {
     var uvIn = currentWeather.uvi;
     var iconCurrent = currentWeather.weather[0].icon;
     var currentIcon = $(document.createElement('img'));
     currentIcon.attr('src', iconPath + iconCurrent + ".png");
     currentIcon.addClass("iconImage");
-
-    $('#weatherTitle').text("Current " + cityName + " Weather");
+    $('#weatherTitle').text("Current Weather");
     $('#weatherTitle').append(currentIcon);
     $('#temp').text("Temp: " + currentWeather.temp +String.fromCharCode(176) + "F");
     $('#wind').text("Wind: " + currentWeather.wind_speed + " MPH");
@@ -82,15 +92,26 @@ function populateWeather() {
             uvEl.removeClass("uVLow uVModerate").addClass("uVHigh")
         }
 }
-//End Get Weather From Lat Lon
 
+//Start Get User Location
+function locationBtnHandler (){
+    var lcn =[];
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  
+  } else { 
+    windowAlert("Geolocation is not supported by this browser."); //NEEDS TO BECOME MODAL
+  }
+}
 
-
+function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    getWeatherFromLocation();
+}
+//End Get User Location
 
 ////END WEATHER FUNCTION////
-
-
-
 
 //////////////: Global Variables Above ://////////////////////////
 
@@ -140,3 +161,4 @@ function populateMainSection(songs) {
 
 searchBtnEl.addEventListener("click", searchBtnHandler)
 weatherBtnEl.addEventListener("click", weatherBtnHandler)
+locationBtnEl.addEventListener("click", locationBtnHandler)
